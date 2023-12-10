2d0cd5a6f863d9d504e835e193438cd1d8f9b091
Star-The-Hentai <slords2016@gmail.com>
2023-12-10 23:08:34 +0200

    Added empty file manipulation files

---
+++ FileManip/AddFile.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import shutil
+import tkinter as tk
+from tkinter import filedialog
+
+def create_vault(vault_path):
+    try:
+        # Check if the vault directory already exists
+        if os.path.exists(vault_path):
+            raise FileExistsError("Vault directory already exists")
+
+        # Create the vault directory
+        os.makedirs(vault_path)
+        print("Vault directory created successfully")
+
+    except FileExistsError as e:
+        print(f"Error: {e}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        # Clean up the partially created vault directory if any error occurs
+        if os.path.exists(vault_path):
+            shutil.rmtree(vault_path)
+        sys.exit(1)
+
+# Create a Tkinter root window
+root = tk.Tk()
+root.withdraw()  # Hide the root window
+
+# Open the file dialog to select a directory
+vault_path = filedialog.askdirectory(title="Select a directory for the vault")
+
+# Check if a directory was selected
+if vault_path:
+    # Call the create_vault function
+    create_vault(vault_path)
+else:
+    print("No directory selected")
