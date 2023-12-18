b011082f7eeb9422f74e593cdce22fb194ec4e67
Omar Mohamed <omarcoptan9@gmail.com>
2023-12-18 19:30:34 +0000

    Add only the needed files for our project

--- main.py
+++ main.py
@@ -4,28 +4,41 @@ import os
 import subprocess
 
 # Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QFileDialog
 from PyQt5.uic import loadUi
 
 # Variables
 cwd = os.getcwd()
+file = cwd + '/tasks/task.txt'
 
+class Fman(QMainWindow):
+    def __init__(self):
+        super(Fman, self).__init__()
+        loadUi('./gui/fman.ui', self)
+        self.browse.clicked.connect(self.browsefiles)
+        
+    def browsefiles(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open File', cwd + '/tasks')
+        self.filename.setText(fname[0])
+        
 
 # Our fancy UI
 class MainUI(QMainWindow):
     def __init__(self):
         super(MainUI, self).__init__()
-        loadUi(cwd + '/gui/mainwin.ui', self)  # Load the .ui file
+        loadUi('./gui/mainwin.ui', self)
         self.setWindowTitle('FMO Task Manager')
+        
+        # Defining functions for buttons
         self.one_btn.clicked.connect(self.one_btn_clicked)
         self.two_btn.clicked.connect(self.two_btn_clicked)
-        # self.three_btn.clicked.connect(self.show_anotherUI)
 
     def one_btn_clicked(self):
-        subprocess.run(["xdg-open", "./tasks/tasks.md"])
+        self.file = Fman()
+        self.file.show()
 
     def two_btn_clicked(self):
-        subprocess.run(["xdg-open", "./tasks/pinned.md"])
+        subprocess.run(["xdg-open", "./tasks/last_visited.md"])
 
 
 # Initialize the app
